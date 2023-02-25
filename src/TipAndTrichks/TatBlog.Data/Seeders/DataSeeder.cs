6b843ef494905d4e2ead8918a0b3e1bde2974873
DuatTran268 <duatsen36@gmail.com>
2023-02-25 16:19:18 +0700

    output table authors

--- src/TipAndTrichks/TatBlog.Data/Seeders/DataSeeder.cs
+++ src/TipAndTrichks/TatBlog.Data/Seeders/DataSeeder.cs
@@ -27,12 +27,238 @@ public class DataSeeder : IDataSeeder
         var posts = AddPosts(authors, categories, tags);
     }
 
-    private IList<Author> AddAuthors() { }
-    private IList<Category> AddCategories() { }
-    private IList<Tag> AddTags() { }
-    private IList<Post> AddPosts (
-        IList<Author> authors,
-        IList<Category> categories,
-        IList<Tag> tags
-        ){}
+    
+    private IList<Author> AddAuthors()
+    {
+        var authors = new List<Author>() {
+            new()
+            {
+                FullNames = "Jason Mouth",
+                UrlSlug = "jason-mouth",
+                Email = "json@gmail.com",
+                JoinedDate = new DateTime(2022, 10, 21)
+            },
+            new()
+            {
+                FullNames = "Jessica Wonder",
+                UrlSlug = "jessica-wonder",
+                Email = "jessica665@gmail.com",
+                JoinedDate = new DateTime(2020, 4, 19)
+            },
+            new()
+            {
+                FullNames = "Leo Messi",
+                UrlSlug = "messi",
+                Email = "messipsg10@gmail.com",
+                JoinedDate = new DateTime(2022, 8, 19)
+            },
+            new()
+            {
+                FullNames = "Cristiano Ronadldo",
+                UrlSlug = "cristiano ronaldo",
+                Email = "ronaldo7@gmail.com",
+                JoinedDate = new DateTime(2022, 8, 7)
+            },
+            new()
+            {
+                FullNames = "Neymar Jr",
+                UrlSlug = "neymar jr",
+                Email = "neymarjs11@gmail.com",
+                JoinedDate = new DateTime(2023, 8, 6)
+            }
+        };
+        _dbContext.Authors.AddRange(authors);
+        _dbContext.SaveChanges();
+
+        return authors;
+    }
+
+    private IList<Category> AddCategories()
+    {
+        var categories = new List<Category>() {
+            new()
+            {
+                Name = ".NET Core",
+                Description = ".NET Core",
+                UrlSlug = "net-corer"
+            },
+            new()
+            {
+                Name = "Architecture",
+                Description = "Architecture",
+                UrlSlug = "architecture"
+            },
+            new()
+            {
+                Name = "Messaging",
+                Description = "Messaging",
+                UrlSlug = "messaging"
+            },
+            new()
+            {
+                Name = "OOP",
+                Description = "OOP",
+                UrlSlug = "oop"
+            },
+            new()
+            {
+                Name = "Design Patterns",
+                Description = "Design Patterns",
+                UrlSlug = "design patterns"
+            },
+            new()
+            {
+                Name = "Javascript",
+                Description = "Javascript Language",
+                UrlSlug = "javascript language"
+            },
+            new()
+            {
+                Name = "C++",
+                Description = "C++ Language",
+                UrlSlug = "c pluss pluss language"
+            },
+            new()
+            {
+                Name = "Python",
+                Description = "python is traslate con tran",
+                UrlSlug = "python python"
+            }
+            ,
+            new()
+            {
+                Name = "React",
+                Description = "Reactjs is a framework",
+                UrlSlug = "reactjs reactjs"
+            },
+            new()
+            {
+                Name = "ABC",
+                Description = "ABC is a ....",
+                UrlSlug = "abc abc abc"
+            }
+        };
+
+        _dbContext.AddRange(categories);
+        _dbContext.SaveChanges();
+
+        return categories;
+    }
+
+
+    private IList<Tag> AddTags()
+    {
+        var tags = new List<Tag>()
+        {
+            new()
+            {
+                Name = "Google",
+                Description = "Google application a",
+                UrlSlug = "google"
+            },
+            new()
+            {
+                Name = "ASP .NET MVC",
+                Description = "ASP .NET MVC",
+                UrlSlug = "asp .net mvc"
+            },
+            new()
+            {
+                Name = "Razor Page",
+                Description = "Razor Page",
+                UrlSlug = "razor page"
+            },
+            new()
+            {
+                Name = "Blazor",
+                Description = "Blazor",
+                UrlSlug = "blazor"
+            },
+            new()
+            {
+                Name = "Deep Learning",
+                Description = "Deep Learning",
+                UrlSlug = "deep learning"
+            },
+            new()
+            {
+                Name = "Neural Learning",
+                Description = "Neural Learning",
+                UrlSlug = "neural learing"
+            },
+            new()
+            {
+                Name = "C",
+                Description = "C language",
+                UrlSlug = "c language"
+            },
+            new()
+            {
+                Name = "Cpp",
+                Description = "C plus plus is a",
+                UrlSlug = "c plus plus"
+            },
+            new()
+            {
+                Name = "abc",
+                Description = "abc is a ...",
+                UrlSlug = "abcacb"
+            },
+        };
+
+        _dbContext.AddRange(tags);
+        _dbContext.SaveChanges();
+        return tags;
+    }
+
+
+    private IList<Post> AddPosts(IList<Author> authors,
+        IList<Category> categories, IList<Tag> tags)
+    {
+        var posts = new List<Post>()
+        {
+            new()
+            {
+                Title = "ASP .NET Core Diagnostic Scenarios",
+                ShortDescrption = "David and friend has a great repository",
+                Description = "Here 's a few great DON'T and DO example",
+                Meta = "David and friend has a great reponsitory ",
+                UrlSlug = "aspnet-core-diagnostic-scenarios",
+                Published= true,
+                PostedDate = new DateTime(2021, 9, 30, 10, 20 , 0),
+                ModifiedDate = null,
+                ViewCount = 10,
+                Author = authors[0],
+                Category = categories[0],
+                Tags = new List<Tag>()
+                {
+                    tags[0]
+                }
+            },
+            new()
+            {
+                Title = "ASP .NET Core Diagnostic Scenarios",
+                ShortDescrption = "David and friend has a great repository",
+                Description = "Here 's a few great DON'T and DO example",
+                Meta = "David and friend has a great reponsitory ",
+                UrlSlug = "aspnet-core-diagnostic-scenarios",
+                Published= true,
+                PostedDate = new DateTime(2022, 9, 30, 10, 20 , 0),
+                ModifiedDate = null,
+                ViewCount = 10,
+                Author = authors[0],
+                Category = categories[0],
+                Tags = new List<Tag>()
+                {
+                    tags[0]
+                }
+            },
+
+        };
+
+        _dbContext.AddRange(posts);
+        _dbContext.SaveChanges();
+
+        return posts;
+    }
 }
--- src/TipAndTrichks/TatBlog.WinApp/Program.cs
+++ src/TipAndTrichks/TatBlog.WinApp/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+// csdl va trang thai cua cac doi tuong
+var context = new BlogDbContext();
+
+// tao doi tuong khoi tao du lieu mau
+var seeder = new DataSeeder(context);
+
+// goi ham initialize de nhap du lieu mau
+seeder.Initialize();
+
+    
+var authors = context.Authors.ToList();
+
+// xuat danh sach cac tac gia ra man hinh
+Console.WriteLine("{0, -4} {1, -30} {2, -30} {3, 12}", "ID", "Full Name", "Email", "Joined Date");
+
+foreach (var author in authors)
+{
+    Console.WriteLine("{0, -4} {1, -30} {2, -30} {3, 12:MM/dd/yyyy}",
+        author.Id, author.FullNames, author.Email, author.JoinedDate);
+}
+
+
