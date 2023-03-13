34f55e318684cad0e3b61523cded7c951bbef18f
DuatTran268 <duatsen36@gmail.com>
2023-03-13 18:48:22 +0700

    lab03HD 1. Create area spend admin

--- src/TipAndTrichks/TatBlog.WebApp/Extensions/RouteExtensions.cs
+++ src/TipAndTrichks/TatBlog.WebApp/Extensions/RouteExtensions.cs
@@ -1,37 +1,50 @@
 ﻿namespace TatBlog.WebApp.Extensions
 {
-    public static class RouteExtensions
-    {
-        public static IEndpointRouteBuilder UseBlogRouters(this IEndpointRouteBuilder endpoint) {
+	public static class RouteExtensions
+	{
+		public static IEndpointRouteBuilder UseBlogRouters(this IEndpointRouteBuilder endpoint)
+		{
 
-            // định nghĩa route template , constraint cho các endpoint kết hợp với các action trong các controller
-            endpoint.MapControllerRoute(
-            name: "posts-by-category",
-            pattern: "blog/category/{slug}",
-            defaults: new { controller = "Blog", action = "Category" });
+			// định nghĩa route template , constraint cho các endpoint kết hợp với các action trong các controller
+			endpoint.MapControllerRoute(
+			name: "posts-by-category",
+			pattern: "blog/category/{slug}",
+			defaults: new { controller = "Blog", action = "Category" });
 
-            endpoint.MapControllerRoute(
-              name: "posts-by-tag",
-              pattern: "blog/tag/{slug}",
-              defaults: new { controller = "Blog", action = "Tag" });
+			endpoint.MapControllerRoute(
+			  name: "posts-by-tag",
+			  pattern: "blog/tag/{slug}",
+			  defaults: new { controller = "Blog", action = "Tag" });
 
-            endpoint.MapControllerRoute(
-              name: "single-post",
-              pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
-              defaults: new { controller = "Blog", action = "Post" });
+			endpoint.MapControllerRoute(
+			  name: "single-post",
+			  pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
+			  defaults: new { controller = "Blog", action = "Post" });
 
 
-            // practice c. 1
-            endpoint.MapControllerRoute(
-                name: "posts-by-author",
-                pattern: "blog/author/{slug}",
-                defaults: new { controller = "Blog", action = "Author" });
+			// practice c. 1
+			endpoint.MapControllerRoute(
+				name: "posts-by-author",
+				pattern: "blog/author/{slug}",
+				defaults: new { controller = "Blog", action = "Author" });
 
-            endpoint.MapControllerRoute(
-              name: "default",
-              pattern: "{controller=Blog}/{action=Index}/{id?}");
 
-            return endpoint;
-        }
-    }
+			endpoint.MapControllerRoute(
+			  name: "single-post",
+			  pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
+			  defaults: new { controller = "Blog", action = "Post" });
+
+			endpoint.MapControllerRoute(
+				name: "admin-area",
+				pattern: "admin/{controller=Dashboard}/{action=Index}/{id?}",
+				defaults: new { area = "Admin" });
+
+
+			endpoint.MapControllerRoute(
+			  name: "default",
+			  pattern: "{controller=Blog}/{action=Index}/{id?}");
+
+			return endpoint;
+		}
+	}
 }
