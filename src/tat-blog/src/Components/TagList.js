8ac85e0152e3a98a0780833dd2123390a4e0c89a
DuatTran268 <duatsen36@gmail.com>
2023-04-03 16:10:27 +0700

    lab05 - HD 4. create TagList and PostItem

---
+++ src/tat-blog/src/Components/TagList.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const TagList = ({tagList}) =>{
+  // Array.isArray(tagList): isArray kiểm tra xem biến có phải là mảng ko, trả về true nếu biến là một mảng, trả false nếu biến ko là mảng
+  if (tagList && Array.isArray(tagList) && tagList.length > 0)
+  return (
+    <>
+      {tagList.map((item, index) => {
+        return(
+          <Link to= {`/blog/tag?slug=${item.name}`}
+          title={item.name}
+          className="btn btn-sm btn-outline-secondary me-1"
+          key={index}>
+            {item.name}
+          </Link>
+        )
+      })}
+    </>
+  )
+
+  else{
+    return (
+      <></>
+    )
+  }
+}
+
+export default TagList;
+
+
