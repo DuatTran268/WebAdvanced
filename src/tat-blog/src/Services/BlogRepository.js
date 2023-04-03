4acd60a92cb2c794bae9183f17e0917a0b411e11
DuatTran268 <duatsen36@gmail.com>
2023-04-03 18:30:53 +0700

    lab05 - HD 6.1 create next and previous page

--- src/tat-blog/src/Services/BlogRepository.js
+++ src/tat-blog/src/Services/BlogRepository.js
@@ -2,12 +2,10 @@ import axios from "axios";
 
 
 
-export async function getPosts (keyword = '', pageSize = 10, pageNumber = 1, sortColumn = '', sortOrder = ''){
+export async function getPosts (keyword = '', pageSize = 5, pageNumber = 1, sortColumn = '', sortOrder = ''){
   try {
-    // call api
-    // const response = await axios.get(`https://localhost:7247/api/posts?keyword=${keyword}
-    // &PageSize=${pageSize}&PageNumber=${pageNumber}&SortColumn=${sortColumn}&SortOrder=${sortOrder}`);
-    const response = await axios.get(`https://localhost:7247/api/posts?PageSize=10&PageNumber=1`);
+    // // call api
+    const response = await axios.get(`https://localhost:7247/api/posts?keyword=${keyword}&PageSize=${pageSize}&PageNumber=${pageNumber}&SortColumn=${sortColumn}&SortOrder=${sortOrder}`);
 
     const data = response.data
     if(data.isSuccess){
