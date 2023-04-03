fd90bd26cedec0864ace4da22a13e8205e154f03
DuatTran268 <duatsen36@gmail.com>
2023-04-03 17:05:06 +0700

    lab04 - HD 5. display data from API

---
+++ src/tat-blog/src/Services/BlogRepository.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+
+
+export async function getPosts (keyword = '', pageSize = 10, pageNumber = 1, sortColumn = '', sortOrder = ''){
+  try {
+    // call api
+    // const response = await axios.get(`https://localhost:7247/api/posts?keyword=${keyword}
+    // &PageSize=${pageSize}&PageNumber=${pageNumber}&SortColumn=${sortColumn}&SortOrder=${sortOrder}`);
+    const response = await axios.get(`https://localhost:7247/api/posts?PageSize=10&PageNumber=1`);
+
+    const data = response.data
+    if(data.isSuccess){
+      return data.result;
+    }
+    else {
+      return null;
+    }
+  }
+  catch(error){
+    console.log('Error', error.message);
+    return null
+  }
+}
